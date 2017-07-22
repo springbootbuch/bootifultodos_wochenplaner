28ae7ffc01a5a4900af60bf58b466e24d0aea4e9
Michael J. Simons <michael@simons.ac>
2017-07-22 17:50:20 +0200

    Enable feign

--- src/main/java/de/bootifultodos/wochenplaner/WochenplanerApplication.java
+++ src/main/java/de/bootifultodos/wochenplaner/WochenplanerApplication.java
@@ -18,6 +18,7 @@ package de.bootifultodos.wochenplaner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
@@ -27,6 +28,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableEurekaClient
+@EnableFeignClients
 public class WochenplanerApplication {
 
 	public static void main(final String... args) {
